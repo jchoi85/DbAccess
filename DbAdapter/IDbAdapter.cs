43f7329ba274107363f92678bb5a365efa0de873
Jason Choi <jfiddy@gmail.com>
2018-05-01 15:05:28 -0700

    initial commit

---
+++ DbAdapter/IDbAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DbAccess.DbAdapter
+{
+    public interface IDbAdapter
+    {
+        IDbCommand Cmd { get; }
+        IDbConnection Conn { get; }
+
+        int ExecuteQuery(string storedProcedure, IDataParameter[] parameters, Action<IDataParameter[]> returnParameters = null);
+        T ExecuteScalar<T>(string storedProcedure, IDataParameter[] parameters);
+        List<T> LoadObject<T>(string storedProcedure, IDbDataParameter[] parameters) where T : class;
+    }
+}
